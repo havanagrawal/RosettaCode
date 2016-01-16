39fca453cdacf9187e3b2909575fb7280a116c33
Havan <havanagrawal@gmail.com>
2016-01-16 17:39:09 +0530

    Remove timing from code

--- src/smallones/PerfectAbundantDeficient.scala
+++ src/smallones/PerfectAbundantDeficient.scala
@@ -1,8 +1,9 @@
-package smallones
-
 /**
  * @author Havan
  */
+
+package smallones
+
 object PerfectAbundantDeficient extends App {
   
   def classify(sumOfDivisors: Int, n: Int) =
@@ -10,15 +11,10 @@ object PerfectAbundantDeficient extends App {
     else if (sumOfDivisors > n) "Abundant"
     else "Perfect"
   
-  def sumOfProperDivisors(n: Int) = (1 until n).toList.filter(n % _ == 0).sum 
-  
-  val time = System.nanoTime  
+  def sumOfProperDivisors(n: Int) = (1 until n).filter(n % _ == 0).sum  
   
   val n = 20000
   val classified = (1 to n).toList.par.groupBy { n => classify(sumOfProperDivisors(n), n) }
-  
-  val totalTime = (System.nanoTime - time)/1000000000
-  println(s"Time taken: $totalTime")
   
   classified.foreach(x => println(x._1 + "\t" + x._2.size))
   
